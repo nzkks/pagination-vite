7d791b886eb8b29a8f3bb5642f96d9fc1123d0ef
nzkks <kkshanthosh@gmail.com>
2024-09-18 17:47:55 +1200

    Extract the Pagination component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
+
 import './App.css';
+import Pagination from './components/Pagination';
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [page, setPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
 
   const fetchProducts = async () => {
-    const response = await fetch(`https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`);
+    const response = await fetch(`https://dummyjson.com/products?limit=10&skip=${currentPage * 10 - 10}`);
     const data = await response.json();
 
     if (data && data.products) {
@@ -20,13 +22,7 @@ function App() {
     fetchProducts();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page]);
-
-  const handlePageSelect = selectedPage => {
-    if (selectedPage >= 1 && selectedPage <= totalPages && selectedPage !== page) {
-      setPage(selectedPage);
-    }
-  };
+  }, [currentPage]);
 
   return (
     <>
@@ -42,25 +38,7 @@ function App() {
             ))}
           </div>
 
-          <div className="pagination">
-            <div onClick={() => handlePageSelect(page - 1)} className={page > 1 ? '' : 'pagination__disable'}>
-              ◀
-            </div>
-            {[...Array(totalPages)].map((_, i) => {
-              return (
-                <div
-                  key={i}
-                  onClick={() => handlePageSelect(i + 1)}
-                  className={page === i + 1 ? 'pagination__selected' : ''}
-                >
-                  {i + 1}
-                </div>
-              );
-            })}
-            <div onClick={() => handlePageSelect(page + 1)} className={page < totalPages ? '' : 'pagination__disable'}>
-              ▶
-            </div>
-          </div>
+          <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
         </>
       )}
     </>
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/prop-types */
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const handlePageChange = selectedPage => {
+    if (selectedPage >= 1 && selectedPage <= totalPages && selectedPage !== currentPage) {
+      onPageChange(selectedPage);
+    }
+  };
+
+  return (
+    <div className="pagination">
+      <div onClick={() => handlePageChange(currentPage - 1)} className={currentPage > 1 ? '' : 'pagination__disable'}>
+        ◀
+      </div>
+      {[...Array(totalPages)].map((_, i) => {
+        return (
+          <div
+            key={i}
+            onClick={() => handlePageChange(i + 1)}
+            className={currentPage === i + 1 ? 'pagination__selected' : ''}
+          >
+            {i + 1}
+          </div>
+        );
+      })}
+      <div
+        onClick={() => handlePageChange(currentPage + 1)}
+        className={currentPage < totalPages ? '' : 'pagination__disable'}
+      >
+        ▶
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
