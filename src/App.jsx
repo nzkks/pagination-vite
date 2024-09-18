30aa3337fdeb33bb33944348daed4638720acd3f
nzkks <kkshanthosh@gmail.com>
2024-09-18 13:10:43 +1200

    more practical server based fetch approach: Usually the data will be fetched from a server

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,24 @@ import './App.css';
 function App() {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const fetchProducts = async () => {
-    const response = await fetch('https://dummyjson.com/products?limit=100');
+    const response = await fetch(`https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`);
     const data = await response.json();
 
     if (data && data.products) {
       setProducts(data.products);
+      setTotalPages(Math.floor(data.total / 10));
     }
   };
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [page]);
 
   const handlePageSelect = selectedPage => {
-    if (selectedPage >= 1 && selectedPage <= products.length / 10 && selectedPage !== page) {
+    if (selectedPage >= 1 && selectedPage <= totalPages && selectedPage !== page) {
       setPage(selectedPage);
     }
   };
@@ -30,7 +32,7 @@ function App() {
       {products.length > 0 && (
         <>
           <div className="products">
-            {products.slice(page * 10 - 10, page * 10).map(product => (
+            {products.map(product => (
               <div key={product.id} className="products__single">
                 <img src={product.thumbnail} alt={product.title} />
                 <p className="products__title">{product.title}</p>
@@ -42,7 +44,7 @@ function App() {
             <div onClick={() => handlePageSelect(page - 1)} className={page > 1 ? '' : 'pagination__disable'}>
               ◀
             </div>
-            {[...Array(products.length / 10)].map((_, i) => {
+            {[...Array(totalPages)].map((_, i) => {
               return (
                 <div
                   key={i}
@@ -53,10 +55,7 @@ function App() {
                 </div>
               );
             })}
-            <div
-              onClick={() => handlePageSelect(page + 1)}
-              className={page < products.length / 10 ? '' : 'pagination__disable'}
-            >
+            <div onClick={() => handlePageSelect(page + 1)} className={page < totalPages ? '' : 'pagination__disable'}>
               ▶
             </div>
           </div>
